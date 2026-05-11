952c490b6a64ce1d713544777dbd9748259970b7
abouguri <abdelaadim.bougurine@gmail.com>
2026-05-12 00:33:06 +0100

    Remove color threshold settings n keep only history and copy features

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import { ThemeToggle } from '@/components/ThemeToggle'
 import { LanguageSelector } from '@/components/LanguageSelector'
 import { CopyButton } from '@/components/CopyButton'
 import { AnalysisHistory } from '@/components/AnalysisHistory'
-import { ColorThemeSettings } from '@/components/ColorThemeSettings'
 import { useTranslation } from '@/lib/i18n'
 import { saveAnalysis } from '@/lib/history'
 
@@ -157,7 +156,6 @@ export default function Home() {
             </div>
             <div className="lg:col-span-1 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 p-4 rounded-none">
               <AnalysisHistory />
-              <ColorThemeSettings />
             </div>
           </div>
         )}
@@ -168,7 +166,6 @@ export default function Home() {
             <div className="lg:col-span-2" />
             <div className="lg:col-span-1 bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 p-4 rounded-none">
               <AnalysisHistory />
-              <ColorThemeSettings />
             </div>
           </div>
         )}
