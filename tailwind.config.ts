d61646d53445b6164052a3c6547d26ee8c97091c
abouguri <abdelaadim.bougurine@gmail.com>
2026-05-12 01:09:13 +0100

    Use native system fonts with proper RTL support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)'],
-        arabic: ['var(--font-cairo)'],
+        sans: 'var(--font-sans)',
       },
       keyframes: {
         pulse: {
